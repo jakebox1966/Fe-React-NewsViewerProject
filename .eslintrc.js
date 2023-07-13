a0e82141c4a4cce799f20dbdde4a9b69fc9da20a
jake <jakebox1966@gmail.com>
2023-07-13 14:41:09 +0900

    React-뉴스뷰어-어플리케이션 (News Viewer) - Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['airbnb', 'prettier'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 'warn',
+        // console.log 사용 시 경고
+        'no-unused-vars': 'error',
+        // 할당되지 않은 변수 있을 시 경고
+    },
+}
