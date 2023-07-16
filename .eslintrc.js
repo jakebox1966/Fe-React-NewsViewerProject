600d73d4813e6d131944b6902b109968439ac0a5
jake <jakebox1966@gmail.com>
2023-07-16 17:42:59 +0900

    React-뉴스뷰어-어플리케이션 (News Viewer) - NewsList 컴포넌트, NewsItem 컴포넌트 생성 =>StyledComponent 적용 - Data Fectch

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['airbnb', 'prettier'],
+    extends: ['react-app', 'prettier'],
     overrides: [
         {
             env: {
---
+++ src/components/NewsList.js
@@ -0,0 +1,69 @@
+/**
+ * API 호출 : 컴포넌트가 화면에 보이는 시점에 API 요청 => useEffect 사용
+ *
+ * ** useEffect에 등록하는 함수에 async를 붙이면 안된다. => useEffect에서 반환해야하는 값은 뒷정리 함수이기 때문이다.
+ *    useEffect 내부에서 async/await를 사용하고 싶다면, 함수 내부에 async 키워드가 붙은 또 다른 함수를 만들어서 사용해야한다.
+ *
+ */
+
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import NewsItem from './NewsItem'
+import axios from 'axios'
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+
+    @media screen and(max-width: 768px) {
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        // async를 사용하는 함수 따로 선언
+
+        const fetchData = async () => {
+            setLoading(true)
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=fd3c663915584bb9a394cd406d50780e',
+                )
+                setArticles(response.data.articles)
+            } catch (e) {
+                console.log(e)
+            }
+            setLoading(false)
+        }
+        fetchData()
+    }, [])
+
+    // 대기 중일 때
+
+    if (loading) {
+        return <NewsListBlock>대기중 ...</NewsListBlock>
+    }
+
+    if (!articles) {
+        return null
+    }
+
+    return (
+        <NewsListBlock>
+            {articles.map((article) => (
+                <NewsItem key={article.url} article={article} />
+            ))}
+        </NewsListBlock>
+    )
+}
+
+export default NewsList
